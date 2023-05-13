894031bf73f3960463d8f2a4d19a2ae36173540e
Luiz Paulo Lima <luiz.lima@comuniki.me>
2023-05-13 17:38:26 -0300

    desafio finalizado

--- src/entregasDesafio/mayara-rocha/pacote.test.js
+++ src/entregasDesafio/mayara-rocha/pacote.test.js
@@ -8,4 +8,35 @@ describe("Teste para o cálculo do pacote de acordo com cada projeto", () => {
 
     expect(result).toEqual("pacote_basico");
   });
+
+  test("Retorna pacote básico com o número total de horas seja exatamente 50", () => {
+    const totalDeHorasPorProjeto = 50;
+
+    const result = calcularPacote(totalDeHorasPorProjeto);
+
+    expect(result).toEqual("pacote_intermediario");
+  });
+
+  test("Retorna pacote intermediário caso o número total de horas seja até 100", () => {
+    const totalDeHorasPorProjeto = 99;
+
+    const result = calcularPacote(totalDeHorasPorProjeto);
+
+    expect(result).toEqual("pacote_intermediario");
+  });
+
+  test("Retorna pacote intermediário com o número total de horas seja exatamente 100", () => {
+    const totalDeHorasPorProjeto = 100;
+
+    const result = calcularPacote(totalDeHorasPorProjeto);
+
+    expect(result).toEqual("pacote_premium");
+  });
+  test("Retorna pacote premium caso o número total de horas seja até 200", () => {
+    const totalDeHorasPorProjeto = 199;
+
+    const result = calcularPacote(totalDeHorasPorProjeto);
+
+    expect(result).toEqual("pacote_premium");
+  });
 });
