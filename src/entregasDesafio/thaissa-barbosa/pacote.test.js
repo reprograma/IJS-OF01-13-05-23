c839527a1636b004553b225afa6d6a683843f3c7
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-05-15 18:32:59 -0300

    feat: pacote teste js

--- src/entregasDesafio/thaissa-barbosa/pacote.test.js
+++ src/entregasDesafio/thaissa-barbosa/pacote.test.js
@@ -11,3 +11,60 @@ describe("Pacote testes", () => {
 });
 
 //COmo tem que ficar
+describe('Calcular pacote para cada projeto', () => {
+    it('Retorna pacote básico caso o número total de horas seja até 50', () => {
+        const totalHoraPorProjeto = 20
+
+        const result = calcularPacote(totalHoraPorProjeto)
+
+        expect(result).toBe('pacote_basico')
+    });
+
+    it('Retorna pacote básico caso o número total de horas seja 50', () => {
+        const totalHoraPorProjeto = 50
+
+        const result = calcularPacote(totalHoraPorProjeto)
+
+        expect(result).toBe('pacote_basico')
+    });
+
+    it('Retorna pacote intermediário caso o número total de horas seja até 100', () => {
+        const totalHoraPorProjeto = 99
+
+        const result = calcularPacote(totalHoraPorProjeto)
+
+        expect(result).toBe('pacote_intermediario')
+    })
+
+    it('Retorna pacote intermediário caso o número total de horas seja 100', () => {
+        const totalHoraPorProjeto = 100
+
+        const result = calcularPacote(totalHoraPorProjeto)
+
+        expect(result).toBe('pacote_intermediario')
+    })
+
+     it('Retorna pacote premium caso o número total de horas seja até 200', () => {
+        const totalHoraPorProjeto = 199
+
+        const result = calcularPacote(totalHoraPorProjeto)
+
+        expect(result).toBe('pacote_premium')
+    })
+
+    it('Retorna pacote premium caso o número total de horas seja 200', () => {
+        const totalHoraPorProjeto = 200
+
+        const result = calcularPacote(totalHoraPorProjeto)
+
+        expect(result).toBe('pacote_premium')
+    })
+
+    it('Retorna que não há pacote disponível', () => {
+        const totalHoraPorProjeto = 201
+
+        const result = calcularPacote(totalHoraPorProjeto)
+
+        expect(result).toBe('Não há pacotes disponíveis')
+    })
+})
