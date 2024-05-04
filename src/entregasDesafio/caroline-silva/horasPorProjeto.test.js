a2823ba12f0fafbb84441b5c567646426ceaf301
Carol Silva <contatocarolinebs@gmail.com>
2024-05-04 16:10:26 -0300

    feat: testes para horasDoProjeto

--- src/entregasDesafio/caroline-silva/horasPorProjeto.test.js
+++ src/entregasDesafio/caroline-silva/horasPorProjeto.test.js
@@ -8,4 +8,12 @@ describe('Testa função calcularHorasDeProjeto', () => {
     const tarefas = ['setup', 'formulario', 'responsividade', 'otimizacao_seo', 'construcao_1_pagina', 'integracao_mailchimp', 'ssr', 'integracao_api_propria'];
     expect(calcularHorasDeProjeto(tarefas)).toBe(104);
   });
+  test('Testa se a função calcularHorasDeProjeto retorna 40 ao colocar setup, formulario e reponsividade', () => {
+    const tarefas = ['setup', 'formulario', 'responsividade', ];
+    expect(calcularHorasDeProjeto(tarefas)).toBe(40);
+  });
+  test('Testa se a função calcularHorasDeProjeto retorna 24 ao colocar construcao_1_pagina, construcao_1_pagina  e construcao_1_pagina', () => {
+    const tarefas = ['construcao_1_pagina', 'construcao_1_pagina', 'construcao_1_pagina' ];
+    expect(calcularHorasDeProjeto(tarefas)).toBe(24);
+  });
 });
