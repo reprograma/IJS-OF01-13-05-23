d86e890d1483be3b0530f9faafbc1ef81e5800f0
Thalia Cavalcanti <thaliascavalcanti@gmail.com>
2024-05-04 17:08:05 -0300

    removendo comentários

--- src/entregasDesafio/thalia-cavalcanti/valorProjeto.test.js
+++ src/entregasDesafio/thalia-cavalcanti/valorProjeto.test.js
@@ -1,12 +1,8 @@
 const { calcularValorTotalProjeto } = require('../../dominio/calculadora/Projeto/valorProjeto');
 const pacote = require('../../dominio/calculadora/Projeto/pacote');
 
-// jest.mock('../../dominio/calculadora/Projeto/pacote.js');
-
 describe('Valor do Projeto', () => {
-    // beforeEach(() => {
-    //     pacote.calcularPacote.mockReturnValue('pacote_basico')
-    // })
+
     test('retorna valor total para um projeto funcionalidades por projeto pedidas e o valor da hora da desenvolvedora', () => {
     const funcionalidades = [
         'setup',
