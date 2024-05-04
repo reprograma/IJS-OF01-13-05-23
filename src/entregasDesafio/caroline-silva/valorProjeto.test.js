6a46db78ceeb584acd22a3549357efc333c3f029
Carol Silva <contatocarolinebs@gmail.com>
2024-05-04 15:43:52 -0300

    adiciona testes para valorProjeto

--- src/entregasDesafio/caroline-silva/valorProjeto.test.js
+++ src/entregasDesafio/caroline-silva/valorProjeto.test.js
@@ -22,4 +22,19 @@ describe('Testa a função calcularValorTotalProjeto', () => {
   test('Se ao passar setup e o número 0 retorna 0', () => {
     expect(calcularValorTotalProjeto(['setup'], 0)).toBe(0);
   });
+  test('Se ao passar formulario e o número 1 retorna 0', () => {
+    expect(calcularValorTotalProjeto(['formulario'], 1)).toBe(18);
+  });
+  test('Se ao passar formulario e o número 10 retorna 176', () => {
+    expect(calcularValorTotalProjeto(['formulario'], 10)).toBe(176);
+  });
+  test('Se ao passar formulario e o número 10.1 retorna 178', () => {
+    expect(calcularValorTotalProjeto(['formulario'], 10.1)).toBe(178);
+  });
+  test('Se ao passar responsividade e o número 1 retorna 18', () => {
+    expect(calcularValorTotalProjeto(['responsividade'], 1)).toBe(18);
+  });
+  test('Se ao passar responsividade e o número 15 retorna 100', () => {
+    expect(calcularValorTotalProjeto(['responsividade'], 15)).toBe(264);
+  });
 });
