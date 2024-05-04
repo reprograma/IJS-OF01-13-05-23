727a6e6bf0fcc108318025fa8ee4eeddaaf18c09
Carol Silva <contatocarolinebs@gmail.com>
2024-05-04 16:15:24 -0300

    fix: ajusta testes

--- src/entregasDesafio/caroline-silva/pacote.test.js
+++ src/entregasDesafio/caroline-silva/pacote.test.js
@@ -10,8 +10,8 @@ describe('Testa a função pacote', () => {
   test('Se ao passar 49.9 retorna pacote_basico', () => {
     expect(calcularPacote(49.9)).toBe('pacote_basico');
   });
-  test('Se ao passar 50 retorna pacote_intermediario', () => {
-    expect(calcularPacote(50)).toBe('pacote_intermediario');
+  test('Se ao passar 50 retorna pacote_basico', () => {
+    expect(calcularPacote(50)).toBe('pacote_basico');
   });
   test('Se ao passar 60 retorna pacote_intermediario', () => {
     expect(calcularPacote(60)).toBe('pacote_intermediario');
@@ -22,13 +22,16 @@ describe('Testa a função pacote', () => {
   test('Se ao passar 99.9 retorna pacote_intermediario', () => {
     expect(calcularPacote(99.9)).toBe('pacote_intermediario');
   });
-  test('Se ao passar 100 retorna pacote_premium', () => {
-    expect(calcularPacote(100)).toBe('pacote_premium');
+  test('Se ao passar 100 retorna pacote_intermediario', () => {
+    expect(calcularPacote(100)).toBe('pacote_intermediario');
   });
   test('Se ao passar 110 retorna pacote_premium', () => {
     expect(calcularPacote(110)).toBe('pacote_premium');
   });
   test('Se ao passar 199 retorna pacote_premium', () => {
+    expect(calcularPacote(199)).toBe('pacote_premium');
+  });
+  test('Se ao passar 200 retorna pacote_premium', () => {
     expect(calcularPacote(199)).toBe('pacote_premium');
   });
 });
